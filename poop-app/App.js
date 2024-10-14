49d1ae021442058d10ba50de2a3ea05ffab54658
Ryoga05 <ryoga20082005@gmail.com>
2024-10-14 12:36:07 +0200

    Actualizar Navigation en App.js

--- poop-app/App.js
+++ poop-app/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Search from './pages/Search';
+import Add from './pages/Add';
+import User from './pages/User';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +16,9 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Add" component={Add} />
+        <Stack.Screen name="User" component={User} />
       </Stack.Navigator>
     </NavigationContainer>
   );
