04b50d4bd5f65e8c6b665b5d2812401467b18a65
Ryoga05 <ryoga20082005@gmail.com>
2024-10-21 11:34:33 +0200

    Añadir AddImage a la pagina de Add.js

--- poop-app/pages/Add.js
+++ poop-app/pages/Add.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+
 import Menu from '../components/Menu'
+import AddImage from '../components/AddImage';
 
 export default function Add() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Bienvenido al Add</Text>
+            <AddImage placeholder={require('../assets/addImage1.jpg')}/>
             <Menu Active="Add"/>
         </View>
     );
