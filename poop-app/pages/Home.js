f2ecbce0cd474ca832c5f71112d894b09d6fd4e2
Ryoga05 <ryoga20082005@gmail.com>
2024-10-28 09:16:18 +0100

    Afegir el Map.js i afegir-ho a la pagina Home

--- poop-app/pages/Home.js
+++ poop-app/pages/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import Menu from '../components/Menu'
+import Map from '../components/Map';
 
 export default function Home() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Bienvenido al Home</Text>
+            <Map/>
             <Menu Active="Home"/>
         </View>
     );
